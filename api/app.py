c66a343dc083789d1e203f7a6fc207491fed4486
adithyasource <me@adithya.zip>
2023-12-26 01:15:41 +0530

    Reverting Back to Previous Asset Finder

--- api/app.py
+++ api/app.py
@@ -42,143 +42,171 @@ def handleRequest():
         response.headers.add("Access-Control-Allow-Origin", "*")
         return response
 
+    # if request.args.get("assets"):
+    #     gameID = str(request.args.get("assets"))
+    #     imageType = str(request.args.get("imageType"))
+    #     imageIndex = int(request.args.get("imageIndex"))
+
+    #     if imageType == "grid":
+    #         gridImageData = json.loads(
+    #             requests.get(
+    #                 f"https://www.steamgriddb.com/api/v2/grids/game/{gameID}",
+    #                 headers={
+    #                     "Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"
+    #                 },
+    #                 timeout=30,
+    #             ).content
+    #         )
+
+    #         gridImageLink = gridImageData["data"][imageIndex]["thumb"]
+
+    #         gridImageFile = requests.get(
+    #             gridImageLink,
+    #             timeout=30,
+    #         )
+
+    #         gridImageFileBytes = list(gridImageFile.content)
+
+    #         response = jsonify({"grid": gridImageFileBytes})
+    #         response.headers.add("Access-Control-Allow-Origin", "*")
+    #         return response
+
+    #     if imageType == "hero":
+    #         heroImageData = json.loads(
+    #             requests.get(
+    #                 f"https://www.steamgriddb.com/api/v2/heroes/game/{gameID}",
+    #                 headers={
+    #                     "Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"
+    #                 },
+    #                 timeout=30,
+    #             ).content
+    #         )
+
+    #         heroImageLink = heroImageData["data"][imageIndex]["thumb"]
+
+    #         heroImageFile = requests.get(
+    #             heroImageLink,
+    #             timeout=30,
+    #         )
+
+    #         heroImageFileBytes = list(heroImageFile.content)
+
+    #         response = jsonify({"hero": heroImageFileBytes})
+    #         response.headers.add("Access-Control-Allow-Origin", "*")
+    #         return response
+
+    #     if imageType == "logo":
+    #         logoImageData = json.loads(
+    #             requests.get(
+    #                 f"https://www.steamgriddb.com/api/v2/logos/game/{gameID}",
+    #                 headers={
+    #                     "Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"
+    #                 },
+    #                 timeout=30,
+    #             ).content
+    #         )
+
+    #         logoImageLink = logoImageData["data"][imageIndex]["thumb"]
+
+    #         logoImageFile = requests.get(
+    #             logoImageLink,
+    #             timeout=30,
+    #         )
+
+    #         logoImageFileBytes = list(logoImageFile.content)
+
+    #         response = jsonify({"logo": logoImageFileBytes})
+    #         response.headers.add("Access-Control-Allow-Origin", "*")
+    #         return response
+
+    #     if imageType == "icon":
+    #         iconImageData = json.loads(
+    #             requests.get(
+    #                 f"https://www.steamgriddb.com/api/v2/icons/game/{gameID}",
+    #                 headers={
+    #                     "Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"
+    #                 },
+    #                 timeout=30,
+    #             ).content
+    #         )
+
+    #         iconImageLink = iconImageData["data"][imageIndex]["thumb"]
+
+    #         iconImageFile = requests.get(
+    #             iconImageLink,
+    #             timeout=30,
+    #         )
+
+    #         iconImageFileBytes = list(iconImageFile.content)
+
+    #         response = jsonify({"icon": iconImageFileBytes})
+    #         response.headers.add("Access-Control-Allow-Origin", "*")
+    #         return response
+
+    #     return "response"
+
     if request.args.get("assets"):
         gameID = str(request.args.get("assets"))
-        imageType = str(request.args.get("imageType"))
-        imageIndex = int(request.args.get("imageIndex"))
 
-        if imageType == "grid":
-            gridImageData = json.loads(
-                requests.get(
-                    f"https://www.steamgriddb.com/api/v2/grids/game/{gameID}",
-                    headers={
-                        "Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"
-                    },
-                    timeout=30,
-                ).content
-            )
+        gridImageLinks = []
+        heroImageLinks = []
+        logoImageLinks = []
+        iconImageLinks = []
 
-            gridImageLink = gridImageData["data"][imageIndex]["thumb"]
-
-            gridImageFile = requests.get(
-                gridImageLink,
+        gridImageData = json.loads(
+            requests.get(
+                f"https://www.steamgriddb.com/api/v2/grids/game/{gameID}",
+                headers={"Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"},
                 timeout=30,
-            )
+            ).content
+        )
 
-            gridImageFileBytes = list(gridImageFile.content)
+        for x in gridImageData["data"]:
+            gridImageLinks.append(x["thumb"])
 
-            response = jsonify({"grid": gridImageFileBytes})
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            return response
-
-        if imageType == "hero":
-            heroImageData = json.loads(
-                requests.get(
-                    f"https://www.steamgriddb.com/api/v2/heroes/game/{gameID}",
-                    headers={
-                        "Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"
-                    },
-                    timeout=30,
-                ).content
-            )
-
-            heroImageLink = heroImageData["data"][imageIndex]["thumb"]
-
-            heroImageFile = requests.get(
-                heroImageLink,
+        heroImageData = json.loads(
+            requests.get(
+                f"https://www.steamgriddb.com/api/v2/heroes/game/{gameID}",
+                headers={"Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"},
                 timeout=30,
-            )
+            ).content
+        )
 
-            heroImageFileBytes = list(heroImageFile.content)
+        for x in heroImageData["data"]:
+            heroImageLinks.append(x["thumb"])
 
-            response = jsonify({"hero": heroImageFileBytes})
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            return response
-
-        if imageType == "logo":
-            logoImageData = json.loads(
-                requests.get(
-                    f"https://www.steamgriddb.com/api/v2/logos/game/{gameID}",
-                    headers={
-                        "Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"
-                    },
-                    timeout=30,
-                ).content
-            )
-
-            logoImageLink = logoImageData["data"][imageIndex]["thumb"]
-
-            logoImageFile = requests.get(
-                logoImageLink,
+        logoImageData = json.loads(
+            requests.get(
+                f"https://www.steamgriddb.com/api/v2/logos/game/{gameID}",
+                headers={"Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"},
                 timeout=30,
-            )
+            ).content
+        )
 
-            logoImageFileBytes = list(logoImageFile.content)
+        for x in logoImageData["data"]:
+            logoImageLinks.append(x["thumb"])
 
-            response = jsonify({"logo": logoImageFileBytes})
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            return response
-
-        if imageType == "icon":
-            iconImageData = json.loads(
-                requests.get(
-                    f"https://www.steamgriddb.com/api/v2/icons/game/{gameID}",
-                    headers={
-                        "Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"
-                    },
-                    timeout=30,
-                ).content
-            )
-
-            iconImageLink = iconImageData["data"][imageIndex]["thumb"]
-
-            iconImageFile = requests.get(
-                iconImageLink,
+        iconImageData = json.loads(
+            requests.get(
+                f"https://www.steamgriddb.com/api/v2/icons/game/{gameID}",
+                headers={"Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"},
                 timeout=30,
-            )
+            ).content
+        )
 
-            iconImageFileBytes = list(iconImageFile.content)
+        for x in iconImageData["data"]:
+            iconImageLinks.append(x["thumb"])
 
-            response = jsonify({"icon": iconImageFileBytes})
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            return response
+        allImages = {
+            "grids": gridImageLinks,
+            "heroes": heroImageLinks,
+            "logos": logoImageLinks,
+            "icons": iconImageLinks,
+        }
 
-        # heroImageData = json.loads(
-        #     requests.get(
-        #         f"https://www.steamgriddb.com/api/v2/heroes/game/{gameID}",
-        #         headers={"Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"},
-        #         timeout=30,
-        #     ).content
-        # )
-
-        # for x in heroImageData["data"]:
-        #     heroImageLinks.append(x["thumb"])
-
-        # logoImageData = json.loads(
-        #     requests.get(
-        #         f"https://www.steamgriddb.com/api/v2/logos/game/{gameID}",
-        #         headers={"Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"},
-        #         timeout=30,
-        #     ).content
-        # )
-
-        # for x in logoImageData["data"]:
-        #     logoImageLinks.append(x["thumb"])
-
-        # iconImageData = json.loads(
-        #     requests.get(
-        #         f"https://www.steamgriddb.com/api/v2/icons/game/{gameID}",
-        #         headers={"Authorization": "Bearer 02469044c89b4c09df44b6a79579018d"},
-        #         timeout=30,
-        #     ).content
-        # )
-
-        # for x in iconImageData["data"]:
-        #     iconImageLinks.append(x["thumb"])
-
-        # response = jsonify(allImages)
-        # response.headers.add("Access-Control-Allow-Origin", "*")
-        return "response"
+        response = jsonify(allImages)
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
 
     if request.args.get("limitedAssets"):
         gameID = str(request.args.get("limitedAssets"))
