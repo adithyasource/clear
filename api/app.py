b98fdbe875fa54e9a93dbd55671dbe2fe3bb9762
adithyasource <me@adithya.zip>
2023-12-27 15:30:46 +0530

    Testing Updates

--- api/app.py
+++ api/app.py
@@ -12,7 +12,7 @@ def handleRequest():
         return "hey there, how'd you end up here? this is the main website: https://clear.adithya.zip"
 
     if request.args.get("version"):
-        response = jsonify({"clearVersion": "0.18.0"})
+        response = jsonify({"clearVersion": "0.19.0"})
         response.headers.add("Access-Control-Allow-Origin", "*")
         return response
 
