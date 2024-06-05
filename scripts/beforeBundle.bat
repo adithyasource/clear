0b93c06cff4c6eccffb6b96874034a696217e17b
adithyasource <adithyasrc@gmail.com>
2024-06-05 23:13:21 +0530

    build: beforeBundle create build dir if not exists

--- scripts/beforeBundle.bat
+++ scripts/beforeBundle.bat
@@ -1,5 +1,10 @@
 @echo off
 upx --ultra-brute src-tauri\target\x86_64-pc-windows-msvc\release\clear.exe
+
+if not exist ".\builds\" mkdir ".\builds\"
+if not exist ".\builds\setup\" mkdir ".\builds\setup\"
+if not exist ".\builds\portable\" mkdir ".\builds\portable\"
+
 makensis "/XOutFile '..\builds\setup\clear v1.1.0 setup.exe'" scripts\installer.nsi
 copy "src-tauri\target\x86_64-pc-windows-msvc\release\clear.exe" "builds\portable\clear v1.1.0 portable.exe" 
 echo ===----------------------------------------------------------------------------------------------------------------------------
