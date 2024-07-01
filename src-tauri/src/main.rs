bad42edbe18111ecddbdbc62104d84e8c90a33bd
adithyasource <adithyasrc@gmail.com>
2024-07-02 00:47:21 +0400

    style: formatted main.rs with rustfmt

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,63 +3,64 @@
 
 use std::fs;
 use std::os::windows::process::CommandExt;
-use std::process::{ Command, Stdio };
-use tauri::{ Manager, Window };
+use std::process::{Command, Stdio};
+use tauri::{Manager, Window};
 
 #[tauri::command]
 fn open_location(location: &str) {
-  let _ = Command::new("cmd")
-    .args(&["/C", "start", "", location])
-    .stdout(Stdio::null())
-    .stderr(Stdio::null())
-    .creation_flags(0x08000000)
-    .spawn();
+    let _ = Command::new("cmd")
+        .args(&["/C", "start", "", location])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .creation_flags(0x08000000)
+        .spawn();
 }
 
 #[tauri::command]
 fn close_app() {
-  std::process::exit(0x0);
+    std::process::exit(0x0);
 }
 
 #[tauri::command]
 fn read_steam_vdf() -> String {
-  match fs::read_to_string("C:\\Program Files (x86)\\Steam\\steamapps\\libraryfolders.vdf") {
-    Ok(file_contents) => file_contents.into(),
-    Err(_err) => {
-      return "error".to_string();
+    match fs::read_to_string("C:\\Program Files (x86)\\Steam\\steamapps\\libraryfolders.vdf") {
+        Ok(file_contents) => file_contents.into(),
+        Err(_err) => {
+            return "error".to_string();
+        }
     }
-  }
 }
 
 #[tauri::command]
 async fn show_window(window: Window) {
-  window.get_window("main").expect("no window labeled 'main' found").show().unwrap();
+    window
+        .get_window("main")
+        .expect("no window labeled 'main' found")
+        .show()
+        .unwrap();
 }
 
 #[tauri::command]
 fn download_image(link: &str, location: &str) {
-  let command_str = format!("Invoke-WebRequest '{}' -Outfile '{}'", link, location);
+    let command_str = format!("Invoke-WebRequest '{}' -Outfile '{}'", link, location);
 
-  let _ = Command::new("powershell")
-    .args(&[command_str])
-    .stdout(Stdio::null())
-    .stderr(Stdio::null())
-    .creation_flags(0x08000000)
-    .output();
+    let _ = Command::new("powershell")
+        .args(&[command_str])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .creation_flags(0x08000000)
+        .output();
 }
 
 fn main() {
-  tauri::Builder
-    ::default()
-    .invoke_handler(
-      tauri::generate_handler![
-        open_location,
-        close_app,
-        read_steam_vdf,
-        show_window,
-        download_image
-      ]
-    )
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            open_location,
+            close_app,
+            read_steam_vdf,
+            show_window,
+            download_image
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
