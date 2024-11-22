c2754bf56f20336e6ec7cb313d7553f61174372f
adithyasource <adithyasrc@gmail.com>
2024-11-23 00:54:10 +0530

    fix: fixed windows build commands

--- scripts/windows_bundle.bat
+++ scripts/windows_bundle.bat
@@ -9,6 +9,6 @@ makensis "/XOutFile '..\builds\setup\clear v1.1.0 setup.exe'" scripts\installer.
 copy "src-tauri\target\x86_64-pc-windows-msvc\release\clear.exe" "builds\portable\clear v1.1.0 portable.exe" 
 echo ===----------------------------------------------------------------------------------------------------------------------------
 echo ===   portable available in %cd%\builds\portable\
-echo ===   setup available in %cd%builds\setup\
+echo ===   setup available in %cd%\builds\setup\
 echo ===   following are the tauri auto generated bundles
 echo ===----------------------------------------------------------------------------------------------------------------------------
